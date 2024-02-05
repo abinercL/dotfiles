37a8375da650c967ca053877261ab09b3973ed79
abinercl <abiner17costa@gmail.com>
2024-02-05 13:25:12 -0300

    finalizado mudança para manajaro

--- scripts/init.sh
+++ scripts/init.sh
@@ -30,3 +30,4 @@ sudo pacman -S base-devel net-tools git github-cli alacritty helix visual-studio
 
 # install asdf
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
+
--- scripts/pkgs.sh
+++ scripts/pkgs.sh
@@ -1,6 +1,4 @@
-sudo apt install cargo python3 nodejs -y
 
-sudo snap install go --classic
+yay -S  zoxide eza bat git-delta xcp rm-improved fd-find  ripgrep --noconfirm
 
-cargo install zoxide eza bat git-delta xcp rm-improved fd-find # ripgrep
-fish_add_path ~/.cargo/bin/
+
